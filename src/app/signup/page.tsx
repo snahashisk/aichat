407ddee78e8baf1972a72e759c25813bf0a0dae3
Snahashis <snahashiskanrar@gmail.com>
2025-12-30 03:36:18 +0530

    login signup page

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { GalleryVerticalEnd } from "lucide-react"
 
-const page = () => {
+import { SignupForm } from "@/components/signup-form"
+
+export default function SignupPage() {
   return (
-    <div>page</div>
+    <div className="grid max-h-full lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <SignupForm />
+          </div>
+        </div>
+      </div>
+      <div className="bg-muted relative hidden lg:block">
+        <img
+          src="/placeholder.svg"
+          alt="Image"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+    </div>
   )
 }
-
-export default page
